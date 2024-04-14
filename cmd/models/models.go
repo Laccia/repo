ac14627e9381a6abb72173e309153a6d75ded454
Laccia <maksim_kalugin_2016@mail.ru>
2024-04-14 17:43:12 +0300

    Update models

--- cmd/models/models.go
+++ cmd/models/models.go
@@ -10,6 +10,27 @@ type Users struct {
 
 type Items struct {
 	Id    uint
-	Name  string
-	Price float64
+	Name  string  `json:"name"`
+	Price float64 `Json:"price"`
+}
+
+type CreateUser struct {
+	Account string `json:"account"`
+	Name    string `json:"name"`
+	Pass    string `json:"pass"`
+	Email   string `json:"email"`
+}
+
+type Login struct {
+	Account string `json:"account"`
+	Pass    string `json:"pass"`
+}
+
+type CreateItem struct {
+	Name  string  `json:"name"`
+	Price float64 `Json:"price"`
+}
+
+type DeleteItem struct {
+	Id uint
 }
